7938a76e3ca515c727839e6922af3171002ef081
sansanua <mazuroleksandrua@gmail.com>
2020-04-25 19:41:41 +0300

    Add fonts and header image

--- frontend/src/Pages/Main/Main.js
+++ frontend/src/Pages/Main/Main.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import cx from 'classnames';
+
+import style from './Main.module.scss';
 
 export default class Main extends Component {
     render() {
-        return <>main</>;
+        return (
+            <div className={cx(style.base)}>
+                <div className={cx(style.imageContainer)}>
+                    <div className={cx(style.container)}>
+                        <div className={cx(style.imageContent)}>
+                            <div className={cx(style.title)}>Поиск и сравнение инвестиционных предложений</div>
+                            <div className={cx(style.subtitle)}>
+                                Выберете способ инвестирования,который подходит именно Вам
+                            </div>
+                            <div>button</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={cx(style.container)}></div>
+            </div>
+        );
     }
 }
--- frontend/src/containers/Header/Header.js
+++ frontend/src/containers/Header/Header.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
+import { useLocation } from 'react-router-dom';
 
 import style from './Header.module.scss';
 
 export default function Header() {
+    const [isTransparent, setIsTransparent] = useState(true);
+    let location = useLocation();
+
+    useEffect(() => {
+        setIsTransparent(location.pathname === '/');
+    }, [location]);
+
     return (
-        <div className={cx(style.base)}>
-            <div className={cx(style.logo)}>logo</div>
-            <div className={cx(style.items)}>
-                <div className={cx(style.item)}>Инструменты</div>
-                <div className={cx(style.item)}>Компании</div>
-                <div className={cx(style.item)}>Персональный советник</div>
-                <div className={cx(style.item)}>О нас</div>
+        <div className={cx(style.base, { [style.transparent]: isTransparent })}>
+            <div className={cx(style.container)}>
+                <div className={cx(style.logo)}></div>
+                <div className={cx(style.items)}>
+                    <div className={cx(style.item)}>Инструменты</div>
+                    <div className={cx(style.item)}>Компании</div>
+                    <div className={cx(style.item)}>Персональный советник</div>
+                    <div className={cx(style.item)}>О нас</div>
+                </div>
+                <div className={cx(style.button)}>=</div>
             </div>
-            <div className={cx(style.button)}>items</div>
         </div>
     );
 }
