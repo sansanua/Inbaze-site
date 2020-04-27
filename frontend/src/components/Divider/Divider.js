3ddee9b46161faa5bd0f8aea77b0bb0562cb9461
sansanua <mazuroleksandrua@gmail.com>
2020-04-27 07:52:31 +0300

    Add footer

---
+++ frontend/src/components/Divider/Divider.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import cx from 'classnames';
+
+import style from './Divider.module.scss';
+
+export default function Divider() {
+    return <div className={cx(style.base)}></div>;
+}
--- frontend/src/containers/Footer/Footer.js
+++ frontend/src/containers/Footer/Footer.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import cx from 'classnames';
 
+import Divider from '../../components/Divider';
+
 import style from './Footer.module.scss';
 
 export default function Footer() {
-    return <div className={cx(style.base)}>Footer</div>;
+    return (
+        <div className={cx(style.base)}>
+            <div className={cx(style.container)}>
+                <div className={cx(style.navigation)}>
+                    <div className={cx(style.logo)}></div>
+
+                    <div className={cx(style.items)}>
+                        <div className={cx(style.item)}>Инструменты</div>
+                        <div className={cx(style.item)}>Компании</div>
+                        <div className={cx(style.item)}>Персональный советник</div>
+                        <div className={cx(style.item)}>О нас</div>
+                    </div>
+
+                    <div className={cx(style.navigationLeftPart)}>Есть вопрос?</div>
+                </div>
+
+                <Divider></Divider>
+
+                <div className={cx(style.contacts)}>
+                    <div className={cx(style.contactsLeft)}>
+                        <div className={cx(style.email)}>info@inbaze.com</div>
+                        <div className={cx(style.phone)}>+380 93 000 00 00</div>
+                    </div>
+                    <div className={cx(style.contactsRight)}>
+                        <div className={cx(style.subscribe)}>Подписантя</div>
+                        <div className={cx(style.socialButtons)}>
+                            <div className={cx(style.socialButton)}>1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
