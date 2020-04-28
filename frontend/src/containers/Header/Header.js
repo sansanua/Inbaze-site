5dcf1c469e36a9df747c4ce1289e5875575e3586
sansanua <mazuroleksandrua@gmail.com>
2020-04-28 08:17:18 +0300

    Add menu for tab and mob

--- frontend/src/containers/Header/Header.js
+++ frontend/src/containers/Header/Header.js
@@ -6,23 +6,30 @@ import style from './Header.module.scss';
 
 export default function Header() {
     const [isTransparent, setIsTransparent] = useState(true);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     let location = useLocation();
 
     useEffect(() => {
         setIsTransparent(location.pathname === '/');
     }, [location]);
 
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className={cx(style.base, { [style.transparent]: isTransparent })}>
             <div className={cx(style.container)}>
                 <div className={cx(style.logo)}></div>
-                <div className={cx(style.items)}>
+                <div className={cx(style.items, { [style.menuOpen]: isMenuOpen })}>
                     <div className={cx(style.item)}>Инструменты</div>
                     <div className={cx(style.item)}>Компании</div>
                     <div className={cx(style.item)}>Персональный советник</div>
                     <div className={cx(style.item)}>О нас</div>
                 </div>
-                <div className={cx(style.button)}>=</div>
+                <div className={cx(style.menu)} onClick={toggleMenu}>
+                    <div className={cx(style.button)}></div>
+                </div>
             </div>
         </div>
     );
