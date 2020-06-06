8b6234388471c8b9e6661a7b943a4cf409cc4d9f
sansanua <mazuroleksandrua@gmail.com>
2020-06-06 12:45:07 +0300

    add mobile foter

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -6,9 +6,11 @@ import style from './Input.module.scss';
 export default function Input({ className, placeholder, label, name, value, onChange }) {
     return (
         <div className={cx(className, style.base)}>
-            <label htmlFor={name} className={cx(style.label)}>
-                {label}
-            </label>
+            {label && (
+                <label htmlFor={name} className={cx(style.label)}>
+                    {label}
+                </label>
+            )}
             <input
                 value={value}
                 type="input"
--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -5,7 +5,6 @@ import cx from 'classnames';
 
 import { CREATE_SUBSCRIBE_NEWS_USER } from 'api/mutations/subscribeNewsUsers';
 
-import Divider from 'components/Divider';
 import Button from 'components/Button/Button';
 import Input from 'components/Input';
 
@@ -13,14 +12,14 @@ import style from './Footer.module.scss';
 
 const socialLinks = [
     {
-        name: 'facebook',
-        logo: '/images/logos/facebook.svg',
-        link: 'https://www.facebook.com/',
-    },
-    {
         name: 'instagram',
         logo: '/images/logos/instagram.svg',
         link: 'https://www.instagram.com/',
+    },
+    {
+        name: 'facebook',
+        logo: '/images/logos/facebook.svg',
+        link: 'https://www.facebook.com/',
     },
     {
         name: 'telegram',
@@ -42,16 +41,38 @@ export default function Footer() {
         <div className={cx(style.base)}>
             <div className={cx(style.container)}>
                 <div className={cx(style.navigation)}>
-                    <div className={cx(style.logo)} />
+                    <div className={style.leftPart}>
+                        <div className={cx(style.logo)} />
 
-                    <div className={cx(style.items)}>
-                        <a href="/companies" className={cx(style.item)}>
-                            Инвестиционные предложения
-                        </a>
-                        <div className={cx(style.item)}>О нас</div>
+                        <div className={cx(style.items)}>
+                            <a href="/companies" className={cx(style.item)}>
+                                Инвестиционные предложения
+                            </a>
+                            <div className={cx(style.item)}>О нас</div>
+                        </div>
+
+                        <div className={cx(style.contacts)}>
+                            <div className={cx(style.contactsSocial)}>
+                                <div className={cx(style.socialButtons)}>
+                                    {socialLinks.map((s) => (
+                                        <a
+                                            className={cx(style.socialButton)}
+                                            href={s.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            <img src={s.logo} alt={s.name} />
+                                        </a>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className={cx(style.contactsEmail)}>
+                                <div className={cx(style.email)}>info@inbaze.com</div>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className={cx(style.navigationLeftPart)}>
+                    <div className={cx(style.navigationRightPart)}>
                         <div className={cx(style.text)}>
                             <div className={cx(style.title)}>Вас интересуют инвестиции ?</div>
                             <div className={cx(style.subtitle)}>
@@ -72,30 +93,6 @@ export default function Footer() {
                                     Подписаться
                                 </Button>
                             </div>
-                        </div>
-                    </div>
-                </div>
-
-                <Divider />
-
-                <div className={cx(style.contacts)}>
-                    <div className={cx(style.contactsLeft)}>
-                        <div className={cx(style.email)}>info@inbaze.com</div>
-                        <div className={cx(style.phone)}>+380 93 000 00 00</div>
-                    </div>
-                    <div className={cx(style.contactsRight)}>
-                        <div className={cx(style.subscribe)}>Подписантя</div>
-                        <div className={cx(style.socialButtons)}>
-                            {socialLinks.map((s) => (
-                                <a
-                                    className={cx(style.socialButton)}
-                                    href={s.link}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <img src={s.logo} alt={s.name} />
-                                </a>
-                            ))}
                         </div>
                     </div>
                 </div>
